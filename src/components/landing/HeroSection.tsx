cb16169a1a544d18714f52cda06a5ac01df8a82f
Darshan <77528189+Dqrshan@users.noreply.github.com>
2026-03-24 08:45:40 +0530

    Update HeroSection.tsx

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -23,7 +23,7 @@ export function HeroSection() {
             seconds: Math.floor((difference / 1000) % 60),
         };
     };
-    const targetDate = new Date("2026-03-23T00:00:00").getTime();
+    const targetDate = new Date("2026-04-04T00:00:00").getTime();
     const [timeLeft, setTimeLeft] = useState<{
         days: number;
         hours: number;
